b4a49e90f6f30d946a21f6f2d31a5dc694604c4c
Joe <112550221+Jrm088@users.noreply.github.com>
2025-08-03 21:31:03 -0400

    fixed incorrect path

--- polybar/launch_polybar.sh
+++ polybar/launch_polybar.sh
@@ -1,7 +1,12 @@
+
+CONFIG="$HOME/terminal-config/polybar/config.ini"
+
 if type "xrandr"; then
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --reload toph &
+    MONITOR=$m polybar --reload toph -c "$CONFIG" &
   done
 else
-  polybar --reload toph &
+  polybar --reload toph -c "$CONFIG" &
 fi
+
+
