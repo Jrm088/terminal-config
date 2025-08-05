ff176d9776da8fc614d182a06e42ed5b9056a549
Joe <112550221+Jrm088@users.noreply.github.com>
2025-08-04 22:02:09 -0400

    created i3 setup script for fresh install & added lxqt config file

---
+++ install-i3-setup.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+set -e
+
+echo "Updating package lists..."
+sudo apt update
+
+echo "Installing essential packages for i3 + LXQt rice..."
+
+sudo apt install -y \
+  betterlockscreen \
+  i3 \
+  i3status \
+  i3lock \
+  i3blocks \
+  imagemagick \
+  x11-xserver-utils \
+  dunst \
+  libnotify-bin \
+  lxappearance \
+  qt5ct \
+  gtk2-engines-murrine \
+  gtk2-engines-pixbuf \
+  kitty \
+  neofetch \
+  pfetch \
+  picom \
+  polybar \
+  sddm \
+  starship \
+  lxqt \
+  lxqt-config \
+  lxqt-panel
+
+echo "Installation complete!"
+
